0374c54862fa6224d6bbf98f09d8f5d5d97a1c2e
Stefan Schlott <stefan.schlott@beone-group.com>
2012-05-24 19:59:59 +0200

    Zugriffskontrolle mit Annotations

--- src/main/java/javamag/dao/ProfileDao.java
+++ src/main/java/javamag/dao/ProfileDao.java
@@ -4,10 +4,14 @@ import java.util.Set;
 
 import javamag.model.Profile;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+
 
 public interface ProfileDao {
+	@RequiresPermissions("profile:read")
 	public abstract Set<String> getAllLogins();
 
+	@RequiresPermissions("profile:read")
 	public abstract Profile findByName(String name);
 
 	public abstract void store(Profile p);
--- src/main/java/javamag/dao/ProfileDaoImpl.java
+++ src/main/java/javamag/dao/ProfileDaoImpl.java
@@ -4,19 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.subject.Subject;
+
 import javamag.model.Profile;
 
 
 public class ProfileDaoImpl implements ProfileDao {
 	private Map<String,Profile> data = new HashMap<String,Profile>();
 	private static ProfileDaoImpl inst = new ProfileDaoImpl();
-	
+
 	public ProfileDaoImpl()  {
 		data.put("admin", new Profile("admin", "Administrator", "Bin hier der Chef"));
 		data.put("adam", new Profile("adam", "Adam Müller", "Hobbies: Hobbygärtner"));
 		data.put("eva", new Profile("eva", "Eva Mayer", "Hobbies: Apfelzucht"));
 	}
-	
+
 	@Override
 	public Set<String> getAllLogins() {
 		return data.keySet();
@@ -24,15 +28,23 @@ public class ProfileDaoImpl implements ProfileDao {
 
 	@Override
 	public Profile findByName(String name) {
-		Profile p = data.get(name); 
+		Profile p = data.get(name);
 		return new Profile(p.getLogin(), p.getName(), p.getDescription());
 	}
 
 	@Override
 	public void store(Profile p) {
-		data.put(p.getLogin(), p);
+		Subject currentUser = SecurityUtils.getSubject();
+		String userName = currentUser.getPrincipal().toString();
+		if (currentUser.isPermitted("profile:write:"+userName) ||
+				(currentUser.isPermitted("profile:write:SELF") &&
+						userName.equals(p.getLogin()))) {
+			data.put(p.getLogin(), p);
+		} else {
+			throw new UnauthorizedException();
+		}
 	}
-	
+
 	public static ProfileDao getInstance() {
 		return inst;
 	}
